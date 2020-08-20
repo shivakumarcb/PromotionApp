617e2f9a50e1958f5d7fff5d5bb8b45d7b2914cb
unknown <shivakumarcb@gmail.com>
2020-08-20 17:14:36 +0530

    added order

---
+++ ConsoleApplication6/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Order
+    {
+        private List<Product> Products;
+
+        public void AddOrders(string item, int quantity)
+        {
+            if (quantity >= 0)
+            {
+                for (int i = 1; i <= quantity; i++)
+                {
+                    Products.Add(new Product(item));
+                }
+                
+            }
+        }
+    }
+}
